276af1f4f706fdee0cf75065e8fd752d1a79019b
przem-pep <przpeplinski@gmail.com>
2024-12-23 11:54:15 +0100

    przekodowano zmienne na typ factor

--- eksploracja-danych.R
+++ eksploracja-danych.R
@@ -11,30 +11,16 @@ dane <- read_csv("czynniki.csv")
 dane
 str(dane)
 
-for(i in 1:ncol(dane)) {
-  print(table(dane[ , i]))
-  cat("\n")
+checkout <- function(x) {
+  for(i in 1:ncol(x)) {
+    print(table(x[ , i]))
+    cat("\n")
+  }
 }
+
+checkout(dane)
 
 # Widoczne jest, że wiele zmiennych należy zmienić na typ factor
-
-# Liczenie braków danych
-
-na_count <- sum(!complete.cases(dane))
-na_count
-
-na_count / nrow(dane)
-
-# Nieco ponad 17% obserwacji posiada braki danych
-
-# Zbadanie liczby braków danych w poszczególnych kolumnach
-
-for(i in 1:ncol(dane)) {
-  cat("Liczba braków danych w kolumnie", names(dane[, i]), ":", sum(is.na(dane[ , i])), "\n")
-}
-
-# Kolumny zawierające braki danych: Sleep_Hours, Family_Income, Teacher_Quality, Parental_Education_Level,
-# Distance_from_Home, Exam_Score
 
 dane$Parental_Involvement <- ifelse(dane$Parental_Involvement == "Low", 1,
                                     ifelse(dane$Parental_Involvement == "Medium", 2, 3))
@@ -57,3 +43,63 @@ dane$Motivation_Level <- factor(dane$Motivation_Level, levels = c(1, 2, 3),
 
 dane$Internet_Access <- ifelse(dane$Internet_Access == "No", 0, 1)
 dane$Internet_Access <- factor(dane$Internet_Access, levels = c(0, 1), labels = c("No", "Yes"))
+
+dane$Family_Income <- ifelse(is.na(dane$Family_Income), NA,
+                             ifelse(dane$Family_Income == "Low", 1,
+                                    ifelse(dane$Family_Income == "Medium", 2, 3)))
+dane$Family_Income <- factor(dane$Family_Income, levels = c(1, 2, 3),
+                             labels = c("Low", "Medium", "High"), ordered = TRUE)
+
+dane$Teacher_Quality <- ifelse(is.na(dane$Teacher_Quality), NA,
+                               ifelse(dane$Teacher_Quality == "Low", 1,
+                                      ifelse(dane$Teacher_Quality == "Medium", 2, 3)))
+dane$Teacher_Quality <- factor(dane$Teacher_Quality, levels = c(1, 2, 3),
+                               labels = c("Low", "Medium", "High"), ordered = TRUE)
+
+dane$School_Type <- ifelse(dane$School_Type == "Public", 1, 2)
+dane$School_Type <- factor(dane$School_Type, levels = c(1, 2), labels = c("Public", "Private"))
+
+dane$Peer_Influence <- ifelse(dane$Peer_Influence == "Negative", 1,
+                              ifelse(dane$Peer_Influence == "Neutral", 2, 3))
+dane$Peer_Influence <- factor(dane$Peer_Influence, levels = c(1, 2, 3),
+                              labels = c("Negative", "Neutral", "Positive"), ordered = TRUE)
+
+dane$Learning_Disabilities <- ifelse(dane$Learning_Disabilities == "No", 0, 1)
+dane$Learning_Disabilities <- factor(dane$Learning_Disabilities, levels = c(0, 1),
+                                     labels = c("No", "Yes"))
+
+dane$Parental_Education_Level <- ifelse(is.na(dane$Parental_Education_Level), NA,
+                                        ifelse(dane$Parental_Education_Level == "High School", 1,
+                                        ifelse(dane$Parental_Education_Level == "College", 2, 3)))
+dane$Parental_Education_Level <- factor(dane$Parental_Education_Level, levels = c(1, 2, 3),
+                                        labels = c("High School", "College", "Postgraduate"),
+                                        ordered = TRUE)
+
+dane$Distance_from_Home <- ifelse(is.na(dane$Distance_from_Home), NA,
+                                  ifelse(dane$Distance_from_Home == "Far", 1,
+                                         ifelse(dane$Distance_from_Home == "Moderate", 2, 3)))
+dane$Distance_from_Home <- factor(dane$Distance_from_Home, levels = c(1, 2, 3),
+                                  labels = c("Far", "Moderate", "Near"), ordered = TRUE)
+
+dane$Gender <- ifelse(dane$Gender == "Female", 1, 2)
+dane$Gender <- factor(dane$Gender, levels = c(1, 2), labels = c("Female", "Male"))
+
+checkout(dane)
+
+# Liczenie braków danych
+
+na_count <- sum(!complete.cases(dane))
+na_count
+
+na_count / nrow(dane)
+
+# Nieco ponad 17% obserwacji posiada braki danych
+
+# Zbadanie liczby braków danych w poszczególnych kolumnach
+
+for(i in 1:ncol(dane)) {
+  cat("Liczba braków danych w kolumnie", names(dane[, i]), ":", sum(is.na(dane[ , i])), "\n")
+}
+
+# Kolumny zawierające braki danych: Sleep_Hours, Family_Income, Teacher_Quality, Parental_Education_Level,
+# Distance_from_Home, Exam_Score
